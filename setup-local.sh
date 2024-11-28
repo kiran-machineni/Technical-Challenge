5af49b900d0c790bd1a398a49b5bfba3dfb5a526
Kiran Kumar Machineni <kiran-machineni@users.noreply.github.com>
2024-11-28 04:59:25 -0500

    fix setup-local

--- setup-local.sh
+++ setup-local.sh
@@ -4,6 +4,7 @@
 set -e
 
 # Prerequisites:
+# 0. Install Docker, Docker compose
 # 1. Install AWS CLI: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 # 2. Authenticate to AWS CLI by running: aws configure
 # 3. Provision DynamoDB by running the terraform workflow or manually create it from AWS CLI
@@ -20,6 +21,11 @@ docker build -t taskmanager-backend:latest ./backend
 
 echo "Building the frontend Docker image..."
 docker build -t taskmanager-frontend:latest ./frontend
+
+# Export AWS credentials into the environment for Docker Compose
+export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+export AWS_REGION=$(aws configure get region)
 
 echo "Starting the Task Manager application using Docker Compose..."
 docker-compose up -d
